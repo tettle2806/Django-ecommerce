f466cb1887c407f66f346160d82eabc773757a2f
tettle2806 <zidanzohid@gmail.com>
2024-01-24 00:22:16 +0500

    models product and categories

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Categories
+from .models import Products
 # Register your models here.
+
+admin.site.register(Categories)
+admin.site.register(Products)
+
